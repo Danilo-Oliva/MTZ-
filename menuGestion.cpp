9af22c8d500dc5870f85b0edf5a4faff4e366671
Martintin1024 <thefraok3010@proton.me>
2025-10-23 13:09:15 -0300

    los menues ppales no titilan
    ahora me falta hacer una funcion con la impresion de los menues. Cuando se vuelven a esos menues no se reimprimen

--- menuGestion.cpp
+++ menuGestion.cpp
@@ -14,6 +14,7 @@
 #include "menuGestionCliente.h"
 #include "menuGestionActividades.h"
 #include "menuGestionInscripciones.h"
+#include "tunearMenu.h"
 
 using namespace std;
 
@@ -24,8 +25,21 @@ void menuActividades()
     ArchivoActividad arch("actividad.dat");
     Actividad act;
     int opcion, y = 0;
+    system("cls");
 
-        system("cls");
+    parteArribaMenu(24, 5, 31);
+    bordesMenu(24, 6, 31);
+    separarMenues(24, 7, 31);
+    for (int i = 0; i < 9; i ++) bordesMenu(24, 8 + i, 31);
+    parteAbajoMenu(24, 17, 31);
+
+
+    escribirTexto("M E N U   A C T I V I D A D E S", 25, 6);
+    escribirTexto("CREAR NUEVA ACTIVIDAD", 30, 9);
+    escribirTexto("MODIFICAR   ACTIVIDAD", 30, 11);
+    escribirTexto(" LISTAR  ACTIVIDADES ", 30, 13);
+    escribirTexto("VOLVER A MENU GESTION", 30, 15);
+
     do
     {
         opcion = -1;
@@ -54,6 +68,19 @@ void menuActividades()
 void menuInscripciones() {
     int opcion, y = 0;
         system("cls");
+    parteArribaMenu(24, 5, 38);
+    bordesMenu(24, 6, 38);
+    separarMenues(24, 7, 38);
+    for (int i = 0; i < 9; i ++) bordesMenu(24, 8 + i, 38);
+    parteAbajoMenu(24, 17, 38);
+
+
+    escribirTexto("M E N U    I N S C R I P C I O N E S", 26, 6);
+    escribirTexto(" INGRESAR NUEVA INSCRIPCION ", 30, 9);
+    escribirTexto("CAMBIAR  ESTADO  INSCRIPCION", 30, 11);
+    escribirTexto(" LISTAR  LAS  INSCRIPCIONES ", 30, 13);
+    escribirTexto("VOLVER   AL   MENU   GESTION", 30, 15);
+
     do {
         opcion = -1;
 
@@ -98,6 +125,20 @@ void accionarMenu()
 {
     int opcionMenu = -1, y = 0;
     system("cls");
+
+    parteArribaMenu(24, 5, 37);
+    bordesMenu(24, 6, 37);
+    separarMenues(24, 7, 37);
+    for (int i = 0; i < 11; i ++) bordesMenu(24, 8 + i, 37);
+    parteAbajoMenu(24, 19, 37);
+
+    escribirTexto("M E N U  D E  G E S T I O N", 30, 6);
+    escribirTexto("GESTIONAR    LOS   CLIENTES", 30, 9);
+    escribirTexto("GESTIONAR  LAS  ACTIVIDADES", 30, 11);
+    escribirTexto("GESTIONAR LAS INSCRIPCIONES", 30, 13);
+    escribirTexto("  REPORTES  DEL  PROGRAMA  ", 30, 15);
+    escribirTexto("  VOLVER AL MENU PRICIPAL  ", 30, 17);
+
     do
     {
 
@@ -119,6 +160,7 @@ void accionarMenu()
             system("cls");
             cout << "Bob el constructor está trabajando en esta opcion." << endl;
             rlutil::anykey();
+            system("cls");
             break;
         }
         if(opcionMenu != 0) opcionMenu = -1;
--- menusVisual.cpp
+++ menusVisual.cpp
@@ -10,18 +10,7 @@ using namespace std;
 
 int mostrarMenuGestion(int &opcionMenu, int &y)
 {
-    parteArribaMenu(24, 5, 37);
-    bordesMenu(24, 6, 37);
-    separarMenues(24, 7, 37);
-    for (int i = 0; i < 11; i ++) bordesMenu(24, 8 + i, 37);
-    parteAbajoMenu(24, 19, 37);
 
-    escribirTexto("M E N U  D E  G E S T I O N", 30, 6);
-    escribirTexto("GESTIONAR    LOS   CLIENTES", 30, 9);
-    escribirTexto("GESTIONAR  LAS  ACTIVIDADES", 30, 11);
-    escribirTexto("GESTIONAR LAS INSCRIPCIONES", 30, 13);
-    escribirTexto("  REPORTES  DEL  PROGRAMA  ", 30, 15);
-    escribirTexto("  VOLVER AL MENU PRICIPAL  ", 30, 17);
 
 
     mostrarCursor(28, 58, 9, y);
@@ -89,18 +78,7 @@ int mostrarMenuClientes(int &opcionMenu, int &y)
 
 int mostrarMenuActividades(int &opcionMenu, int &y)
 {
-    parteArribaMenu(24, 5, 31);
-    bordesMenu(24, 6, 31);
-    separarMenues(24, 7, 31);
-    for (int i = 0; i < 9; i ++) bordesMenu(24, 8 + i, 31);
-    parteAbajoMenu(24, 17, 31);
 
-
-    escribirTexto("M E N U   A C T I V I D A D E S", 25, 6);
-    escribirTexto("CREAR NUEVA ACTIVIDAD", 30, 9);
-    escribirTexto("MODIFICAR   ACTIVIDAD", 30, 11);
-    escribirTexto(" LISTAR  ACTIVIDADES ", 30, 13);
-    escribirTexto("VOLVER A MENU GESTION", 30, 15);
 
 
     mostrarCursor(28, 52, 9, y);
@@ -138,18 +116,6 @@ int mostrarMenuActividades(int &opcionMenu, int &y)
 
 int mostrarMenuInscripciones(int &opcionMenu, int &y)
 {
-    parteArribaMenu(24, 5, 38);
-    bordesMenu(24, 6, 38);
-    separarMenues(24, 7, 38);
-    for (int i = 0; i < 9; i ++) bordesMenu(24, 8 + i, 38);
-    parteAbajoMenu(24, 17, 38);
-
-
-    escribirTexto("M E N U    I N S C R I P C I O N E S", 26, 6);
-    escribirTexto("REGISTRAR NUEVA INSCRIPCION ", 30, 9);
-    escribirTexto("CAMBIAR  ESTADO  INSCRIPCION", 30, 11);
-    escribirTexto(" LISTAR  LAS  INSCRIPCIONES ", 30, 13);
-    escribirTexto("VOLVER   AL   MENU   GESTION", 30, 15);
 
 
     mostrarCursor(28, 60, 9, y);
